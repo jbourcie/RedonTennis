3ba798d40651dd502a5b56a3de58297e3b3dd334
jbourcie <johann.bourcier@irisa.fr>
2019-09-04 18:53:00 +0200

    initial commit

---
+++ docs/js/leaflet-map.js
@@ -0,0 +1,25 @@
+function initMap() {
+//    var latLong = [47.667972, -2.074901];
+
+//    var map = L.map("map").setView(latLong, 14);
+
+//    L.tileLayer("https://b.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+//      maxZoom: 18,
+//    }).addTo(map);
+
+//     L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+//     attribution: 'donn&eacute;es &copy; <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+//     minZoom: 1,
+//     maxZoom: 20
+// }).addTo(map);
+
+    var icon = L.icon({
+      iconUrl: "images/map-marker.png",
+      iconSize:     [25, 41],
+      //iconUrl: "images/map-loc.png",
+      //iconSize:     [57, 66],
+    });
+    //var marker = L.marker(latLong, { icon: icon }).addTo(map);
+}
+//document.addEventListener("DOMContentLoaded", initMap);
